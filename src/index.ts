e1c24ac955b59a087e23a6162e9ac73ea0730951
chenjoachim <lllion66666@gmail.com>
2024-09-07 13:07:50 +0800

    Add inner-join and test api for relations

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import {neon} from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import { InsertActivity, InsertProposal, proposalsTable, activitiesTable } from './schema.js';
+import { eq } from 'drizzle-orm';
 import dotenv from 'dotenv'
 
 dotenv.config();
@@ -62,6 +63,27 @@ app.post('/createActivity', async (req, res) => {
     res.status(201).json(newActivity[0]);
   } catch (error) {
     console.error('Error adding user:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
+
+app.get('/test', async (req, res) => {
+  try {
+    const [result] = await db.select({
+      activity: activitiesTable,
+      proposal: proposalsTable,
+    })
+    .from(activitiesTable)
+    .innerJoin(proposalsTable, eq(activitiesTable.proposalId, proposalsTable.id))
+    .where(eq(activitiesTable.id, "0ce544f4-14fa-4816-9b2f-510749e35655"))
+    .limit(1);
+  
+    if (result) {
+      res.send([{Activity: result.activity.description, Proposal: result.proposal.description}])
+    } else {
+      res.status(404).json({ error: 'No activity found with the given ID' });
+    }
+  } catch(e) {
     res.status(500).json({ error: 'Internal server error' });
   }
 })
